f7fbbb3895eb9928dc54cec55d0197a62b4db4ce
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-01-21 20:05:43 -0300

    perfil de teste implementado

--- src/main/java/com/tiago/bookstore/BookstoreApplication.java
+++ src/main/java/com/tiago/bookstore/BookstoreApplication.java
@@ -1,43 +1,13 @@
 package com.tiago.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.tiago.bookstore.domain.Categoria;
-import com.tiago.bookstore.domain.Livro;
-import com.tiago.bookstore.repositories.CategoriaRepository;
-import com.tiago.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private LivroRepository livroRepository;
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
 	}
 
 }
