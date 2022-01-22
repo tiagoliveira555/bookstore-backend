475e6ecb56d18a4a7b2021785b643b191d1f4b5f
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-01-22 17:10:24 -0300

    ajuste finalizado no backend

--- src/main/java/com/tiago/bookstore/resources/LivroResource.java
+++ src/main/java/com/tiago/bookstore/resources/LivroResource.java
@@ -3,8 +3,11 @@ package com.tiago.bookstore.resources;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +24,7 @@ import com.tiago.bookstore.domain.Livro;
 import com.tiago.bookstore.dtos.LivroDTO;
 import com.tiago.bookstore.services.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
@@ -43,27 +47,27 @@ public class LivroResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro livro) {
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @Valid @RequestBody Livro livro) {
 		Livro newLivro = service.update(id, livro);
 		return ResponseEntity.ok().body(newLivro);
 	}
 
 	@PatchMapping(value = "/{id}")
-	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro livro) {
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @Valid @RequestBody Livro livro) {
 		Livro newLivro = service.update(id, livro);
 		return ResponseEntity.ok().body(newLivro);
 	}
 
 	@PostMapping
 	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
-			@RequestBody Livro obj) {
+			@Valid @RequestBody Livro obj) {
 
 		Livro newObj = service.create(id_cat, obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}")
 				.buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
