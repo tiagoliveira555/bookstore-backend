150d3693b87846d84f1419b49acf2b438838c1bc
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-01-22 16:33:08 -0300

    POST para Livro

--- src/main/java/com/tiago/bookstore/resources/LivroResource.java
+++ src/main/java/com/tiago/bookstore/resources/LivroResource.java
@@ -1,5 +1,6 @@
 package com.tiago.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tiago.bookstore.domain.Livro;
 import com.tiago.bookstore.dtos.LivroDTO;
@@ -23,29 +26,41 @@ public class LivroResource {
 
 	@Autowired
 	private LivroService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Livro> findById(@PathVariable Integer id) {
 		Livro obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+	public ResponseEntity<List<LivroDTO>> findAll(
+			@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
 		List<Livro> list = service.findAll(id_cat);
 		List<LivroDTO> listDTO = list.stream().map(x -> new LivroDTO(x)).toList();
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro livro) {
 		Livro newLivro = service.update(id, livro);
 		return ResponseEntity.ok().body(newLivro);
 	}
-	
+
 	@PatchMapping(value = "/{id}")
 	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro livro) {
 		Livro newLivro = service.update(id, livro);
 		return ResponseEntity.ok().body(newLivro);
 	}
+
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
+			@RequestBody Livro obj) {
+
+		Livro newObj = service.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}")
+				.buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/tiago/bookstore/services/LivroService.java
+++ src/main/java/com/tiago/bookstore/services/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tiago.bookstore.domain.Categoria;
 import com.tiago.bookstore.domain.Livro;
 import com.tiago.bookstore.repositories.LivroRepository;
 import com.tiago.bookstore.services.exceptions.ObjectNotFoundException;
@@ -41,4 +42,11 @@ public class LivroService {
 		newLivro.setNome_autor(livro.getNome_autor());
 		newLivro.setTexto(livro.getTexto());
 	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
+	}	
 }
